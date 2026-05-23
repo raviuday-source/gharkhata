351f828fbcc5da0db6891051ccbb15eebc855191
raviuday-source <raviuday-source@users.noreply.github.com>
2026-05-23 17:14:19 +0530

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 window.GHARKHATA_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBJVKbHjmkLsfpG7IfRA80aeVBmUK298xU",
+  authDomain: "gharkhata-8c827.firebaseapp.com",
+  projectId: "gharkhata-8c827",
+  storageBucket: "gharkhata-8c827.firebasestorage.app",
+  messagingSenderId: "311013167414",
+  appId: "1:311013167414:web:382bbdf1fadec979ea2397",
+  measurementId: "G-ZTN30BLRPM"
 };
 
 window.GHARKHATA_HOUSEHOLD_ID = "ghar-khata-home";
